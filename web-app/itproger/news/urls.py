851dc2a785d01ddd57cf9d56c83e4f05ca49ace8
Renat Kubatiev <renat_888@inbox.ru>
2023-04-02 19:11:43 +0300

    bad

--- web-app/itproger/news/urls.py
+++ web-app/itproger/news/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from . import views
+from .import views
 urlpatterns = [
     path('',views.news_home, name='news_home'),
     path('create',views.create, name='create'),
-    path('<int:pk>',views.NewsDetailView.as_view(), name='news-detail')
+    path('<int:pk>',views.NewsDetailView.as_view())
 ]
